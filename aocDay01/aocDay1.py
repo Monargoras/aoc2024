b071a5c1bd9a93e9e02253ab99e45b8492e49a83
Felix Wagner <felixwagner99@googlemail.com>
2024-12-01 13:35:42 +0100

    add day1

--- aocDay01/aocDay1.py
+++ aocDay01/aocDay1.py
@@ -1,12 +1,43 @@
-def partOne():
-    with open('./aocDay01/input.txt') as input:
-        pass
+def partOne(filePath):
+    with open(filePath) as input:
+        # split in lines
+        lines = input.readlines()
+        # split each line at spaces
+        lines = [line.split() for line in lines]
+        # get first of each line
+        leftList = [line[0] for line in lines]
+        # get second of each line
+        rightList = [line[1] for line in lines]
+        # sort the lists
+        leftList.sort()
+        rightList.sort()
+        # get sum of difference between each pair of numbers
+        sum = 0
+        for i in range(len(leftList)):
+            sum += abs(int(rightList[i]) - int(leftList[i]))
+        print(sum)
 
 
-def partTwo():
-    with open('./aocDay01/input.txt') as input:
-        pass
+def partTwo(filePath):
+    with open(filePath) as input:
+        # split in lines
+        lines = input.readlines()
+        # split each line at spaces
+        lines = [line.split() for line in lines]
+        # get first of each line
+        leftList = [line[0] for line in lines]
+        # get second of each line
+        rightList = [line[1] for line in lines]
+        # get sum of difference between each pair of numbers
+        similarityScore = 0
+        for i in range(len(leftList)):
+            value = int(leftList[i]) * rightList.count(leftList[i])
+            similarityScore += value
+        print(similarityScore)
 
 
 if __name__ == '__main__':
-    partOne()
+    #partOne('./aocDay01/examplePartOne.txt')
+    #partOne('./aocDay01/input.txt')
+    #partTwo('./aocDay01/examplePartTwo.txt')
+    partTwo('./aocDay01/input.txt')
