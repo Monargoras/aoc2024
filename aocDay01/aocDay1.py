6641d966eab4847e95de60eb952672cab5cb07a3
Felix Wagner <felixwagner99@googlemail.com>
2024-12-01 12:29:47 +0100

    init day 1

---
+++ aocDay01/aocDay1.py
@@ -0,0 +1,12 @@
+def partOne():
+    with open('./aocDay01/input.txt') as input:
+        pass
+
+
+def partTwo():
+    with open('./aocDay01/input.txt') as input:
+        pass
+
+
+if __name__ == '__main__':
+    partOne()
