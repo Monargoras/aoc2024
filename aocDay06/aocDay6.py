54aeb779d62499f9f9954a6c3c528846bbfeea75
Felix Wagner <felixwagner99@googlemail.com>
2024-12-07 13:15:31 +0100

    add day6 WIP

--- aocDay06/aocDay6.py
+++ aocDay06/aocDay6.py
@@ -7,8 +7,8 @@ def partOne(filePath):
         directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
         direction = 0
         x, y = coordinates
-        map[y][x] = 'X'
         while True:
+            map[y][x] = 'X'
             if y <= 0 or y >= len(map) - 1 or x <= 0 or x >= len(map[y]) - 1:
                 break
             nextX = x + directions[direction][0]
@@ -19,14 +19,33 @@ def partOne(filePath):
                 nextY = y + directions[direction][1]
             x += directions[direction][0]
             y += directions[direction][1]
-            map[y][x] = 'X'
         xCount = sum(row.count('X') for row in map)
         print(xCount)
 
 
 def partTwo(filePath):
     with open(filePath) as input:
-        pass
+        map = input.readlines()
+        map = [list(x.strip()) for x in map]
+        # find guard position (^) with x and y coordinates
+        coordinates = next((x, y) for y, row in enumerate(map) for x, char in enumerate(row) if char == '^')
+        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+        direction = 0
+        x, y = coordinates
+        listOfRelevantObstructions = []
+        while True:
+            map[y][x] = 'X'
+            if y <= 0 or y >= len(map) - 1 or x <= 0 or x >= len(map[y]) - 1:
+                break
+            nextX = x + directions[direction][0]
+            nextY = y + directions[direction][1]
+            while map[nextY][nextX] == '#':
+                listOfRelevantObstructions.append((nextX, nextY))
+                direction = (direction + 1) % 4
+                nextX = x + directions[direction][0]
+                nextY = y + directions[direction][1]
+            x += directions[direction][0]
+            y += directions[direction][1]
 
 
 if __name__ == '__main__':
